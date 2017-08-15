cb531d2ab3c304441e4ab347e084749e2196a706
Renato Mussinato <mussinato@gmail.com>
2017-08-15 17:28:00 -0300

    Alteração de + para -

--- AulaIntegracaoContinua/src/main/java/com/unipar/mussinato/aulaintegracaocontinua/Calculadora.java
+++ AulaIntegracaoContinua/src/main/java/com/unipar/mussinato/aulaintegracaocontinua/Calculadora.java
@@ -2,6 +2,6 @@ package com.unipar.mussinato.aulaintegracaocontinua;
 
 public class Calculadora {
     public Double somar(Double a, Double b){
-        return a + b;
+        return a - b;
     }
 }
